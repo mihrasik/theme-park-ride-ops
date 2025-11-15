d6174f95ede9b86422ddfee37a4c90735756f713
rahularyanab <rahul.aryanab@gmail.com>
2025-11-15 13:26:28 +0100

    feat: Update JDK installation script to support multiple architectures

--- shell_files/amd64/install_jdk.sh
+++ shell_files/amd64/install_jdk.sh
@@ -1,9 +1,15 @@
 sudo apt update
 sudo apt install -y openjdk-11-jdk
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+
+ARCH="$(uname -m)"
+case "$ARCH" in
+  x86_64) export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 ;;
+  aarch64|arm64) export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-arm64 ;;
+  *) echo "Unsupported architecture: $ARCH" >&2; exit 1 ;;
+esac
 export PATH=$JAVA_HOME/bin:$PATH
 
-echo 'export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64' >> ~/.bashrc
+echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
 
