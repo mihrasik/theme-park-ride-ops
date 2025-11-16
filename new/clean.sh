4bbc8057716691b1024f2d293500e05ca836869c
rahularyanab <rahul.aryanab@gmail.com>
2025-11-16 18:32:54 +0100

    feat: add cleanup and docker service management scripts, and document commands for Kubernetes and Terraform

--- new/clean.sh
+++ new/clean.sh
@@ -1,7 +1,8 @@
 pkill kubectl
 k3d cluster list
-k3d cluster delete <cluster-name>  # repeat for each listed cluster
+k3d cluster delete themepark  # repeat for each listed cluster
 docker ps -q | xargs -r docker stop
 docker ps -aq | xargs -r docker rm
 sudo systemctl stop docker.service
 sudo systemctl disable docker.service
+sudo systemctl status docker.service
