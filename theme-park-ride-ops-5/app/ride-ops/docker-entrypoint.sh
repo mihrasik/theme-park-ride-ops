ed6a1ffe64410f926a49062120c90f75d2beb326
rahularyanab <rahul.aryanab@gmail.com>
2025-11-16 18:41:43 +0100

    deleting unused files

--- theme-park-ride-ops-5/app/ride-ops/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Start SSH daemon in the background
-/usr/sbin/sshd -D &
-# Wait a moment for sshd to bind to the port
-sleep 1
-# Execute the Spring‑Boot application
-exec java -jar /app/theme-park-ride-gradle.jar
