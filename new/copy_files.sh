b9cf4524c55a8ebe7770fafa6f174e3b9e3d126d
Mykhailo <job.dmy@gmail.com>
2025-12-16 10:36:00 +0100

    bash script to copy already downloaded gradle package to increase the speed of the deployment

---
+++ new/copy_files.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p ~/.gradle/wrapper/dists/gradle-7.3.2-bin/4k4cn06q0rruwh9dpndf9gmi8/
+cp ~/shared/files/gradle-7.3.2-bin.zip ~/.gradle/wrapper/dists/gradle-7.3.2-bin/4k4cn06q0rruwh9dpndf9gmi8/
